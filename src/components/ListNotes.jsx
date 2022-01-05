0bbe8dcb461cbaea2b78bd87a33bdfa366498454
MateusSilva42 <mateus.silva@bycoders.com.br>
2022-01-05 11:26:31 -0300

    create listnotes component

---
+++ src/components/ListNotes.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+
+export class ListNotes extends Component {
+  render() {
+    return (
+      <section>
+        <ul>
+          <li>
+            <section>
+              <header>
+                <h3>Título da nota</h3>
+              </header>
+              <p>Texto da sua nota...</p>
+            </section>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <section>
+              <header>
+                <h3>Título da nota</h3>
+              </header>
+              <p>Texto da sua nota...</p>
+            </section>
+          </li>
+        </ul>
+        <ul>
+          <li>
+            <section>
+              <header>
+                <h3>Título da nota</h3>
+              </header>
+              <p>Texto da sua nota...</p>
+            </section>
+          </li>
+        </ul>
+      </section>
+    );
+  }
+}
